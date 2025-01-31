67b5ac5f114e73348396c3d0085d7bf4c037b6b0
Kane Kriz <kanemkriz@gmail.com>
2025-01-30 17:09:30 -0700

    Update code.js

--- code.js
+++ code.js
@@ -7,17 +7,16 @@
 //fibbonaci function
 function fib(n)
 {
-    if(n <= 0)
+if(n < 0)
     {
     return [];
-    }
-    var arr = [];
-    
-    if(n === 1) 
+    } 
+var arr;
+    if(n === 0) 
     {
         return arr;
     }
-    var index = 1; 
+var index = 1; 
     while(index < n)
     {
         if(index < 2)
@@ -28,7 +27,7 @@ function fib(n)
         {
         arr.push(arr[index-1] + arr[index-2]);
         }
-    index++;
-    }
+index++;
+}
 return arr;
 }

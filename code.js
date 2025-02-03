89c8a24bec4732d5b3fbbafa23a65909ea3dd67a
Kane Kriz <kanemkriz@gmail.com>
2025-02-03 10:38:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -4,8 +4,33 @@
 //
 //
 
+
 //fibbonaci function
 function fib(n)
 {
-//nuking this. I was stupid and did not use recursion as specified
+    var currentIteration = 0;
+    var fibArr = [];
+
+    function recHelper()
+    {
+        if(currentIteration > n) //it has reached the desired index
+        {
+            return fibArr;
+        }
+        if(currentIteration === 0) 
+        {
+            fibArr.push(0);
+        } 
+        else if(currentIteration === 1) 
+        {
+            fibArr.push(1);
+        } 
+        else 
+        {
+            fibArr.push(fibArr[currentIteration - 1] + fibArr[currentIteration - 2]);
+        }
+        currentIteration++;
+        return recHelper();
+    }
+    return recHelper();
 }
